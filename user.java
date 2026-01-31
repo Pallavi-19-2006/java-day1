047cabdfdf0839acf24b3395d2c5f8aa048aa817
Pallavi-19-2006 <pallupallavi8595@gmail.com>
2026-01-31 16:06:03 +0530

    Add files via upload

---
+++ user.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class user
+{
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter marks");
+        int marks=obj.nextInt();
+        System.out.println("Enter a");
+        double a=obj.nextDouble();
+        System.out.println("Enter b");
+        float b=obj.nextFloat();
+         System.out.println("Enter str1");
+        String str1=obj.next();
+         System.out.println("Enter str2");
+        String str2=obj.nextLine();
+    }
+}
