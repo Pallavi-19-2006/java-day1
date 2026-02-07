f97bf423a21c8258823cafbbc1cd4b10763892f8
Pallavi-19-2006 <pallupallavi8595@gmail.com>
2026-02-07 15:16:48 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Swapping 
+{
+    public static void main(String args[])
+    {
+        int a=6;
+        int b=9;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+
+    }    
+}
