56dd27b1767910445d45b36a556578a73f4b85b8
Pallavi-19-2006 <pallupallavi8595@gmail.com>
2026-02-07 11:09:20 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Primitive {
+    public static void main(String args[])
+    {
+       int telugu=70;
+       int english=75;
+       int math=60;
+       int sum=telugu+english+math;
+       float ff=sum/3;
+       System.out.println(ff);
+       System.out.println(sum);
+
+    }
+    
+}
