6010a0334b5463b8ba26abafefdd54ff575ea0dc
Pallavi-19-2006 <pallupallavi8595@gmail.com>
2026-02-07 14:01:28 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class Arithmetic 
+{
+    public static void main(String[] args)
+    {
+        int a=45;
+        int b=54;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);    
+    }
+
+    
+}
