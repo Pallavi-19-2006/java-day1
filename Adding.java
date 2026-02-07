a5f406ab33d72b3288bbd4146944bda31eff39fe
Pallavi-19-2006 <pallupallavi8595@gmail.com>
2026-02-07 15:38:57 +0530

    Add files via upload

---
+++ Adding.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Adding 
+{
+    public static void main(String args[])
+    {
+        int a=7;
+        int b=4;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
